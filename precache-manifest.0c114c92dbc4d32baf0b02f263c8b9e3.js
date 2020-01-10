2e2f26838565856330a5d7b9a62a667dfc975f9f
maxw2 <635433387@qq.com>
2020-01-10 09:33:28 +0800

    Updates

---
+++ precache-manifest.0c114c92dbc4d32baf0b02f263c8b9e3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c5ebc383768989487d1addba11f34a74",
+    "url": "./index.html"
+  },
+  {
+    "revision": "3c40f5fa8acb40eaf588",
+    "url": "./static/css/main.261888bd.chunk.css"
+  },
+  {
+    "revision": "83f2a55ca996be7f3eb0",
+    "url": "./static/js/2.6e78db03.chunk.js"
+  },
+  {
+    "revision": "3c40f5fa8acb40eaf588",
+    "url": "./static/js/main.cc56b5fc.chunk.js"
+  },
+  {
+    "revision": "27ebbdb637a0bf2979a7",
+    "url": "./static/js/runtime-main.1653be10.js"
+  },
+  {
+    "revision": "b18ee1a2c801a83f2affcf99a68c0c71",
+    "url": "./static/media/userImg.b18ee1a2.jpg"
+  }
+]);
